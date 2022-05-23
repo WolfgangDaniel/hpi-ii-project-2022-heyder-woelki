14d357bf84ba0ff93c8d2a2a73b03902ab605cb3
bjoeh98 <bjoeh98@mi.fu-berlin.de>
2022-05-23 19:34:04 +0200

    build scraper

--- build/gen/bakdata/lobby/v1/lobby_pb2.py
+++ build/gen/bakdata/lobby/v1/lobby_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1c\x62\x61kdata/lobby/v1/lobby.proto\x12\x10\x62\x61kdata.lobby.v1\"\x84\x01\n\x05Lobby\x12\n\n\x02id\x18\x01 \x01(\t\x12\r\n\x05rb_id\x18\x02 \x01(\r\x12\r\n\x05state\x18\x03 \x01(\t\x12\x14\n\x0creference_id\x18\x04 \x01(\t\x12\x12\n\nevent_date\x18\x05 \x01(\t\x12\x12\n\nevent_type\x18\x06 \x01(\t\x12\x13\n\x0binformation\x18\x08 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1c\x62\x61kdata/lobby/v1/lobby.proto\x12\x10\x62\x61kdata.lobby.v1\"\xfd\x03\n\x05Lobby\x12\n\n\x02id\x18\x01 \x01(\t\x12\x13\n\x0b\x63ompanyName\x18\x02 \x01(\t\x12\x46\n\x12lobbyEmployeeCount\x18\x03 \x01(\x0b\x32*.bakdata.lobby.v1.Lobby.LobbyEmployeeCount\x12\x41\n\x12lobbyEmployyeNames\x18\x04 \x03(\x0b\x32%.bakdata.lobby.v1.Lobby.EmployeeNames\x12\x19\n\x11reasonForLobbying\x18\x05 \x01(\t\x12\x13\n\x0bmemberships\x18\x06 \x03(\t\x12\x10\n\x08\x64onators\x18\x07 \x03(\t\x12\x11\n\tinterests\x18\x08 \x03(\t\x12@\n\x0flobbyMoneySpent\x18\t \x01(\x0b\x32\'.bakdata.lobby.v1.Lobby.LobbyMoneySpent\x1a/\n\x12LobbyEmployeeCount\x12\r\n\x05start\x18\x01 \x01(\x05\x12\n\n\x02to\x18\x02 \x01(\x05\x1a\x34\n\rEmployeeNames\x12\x11\n\tfirstName\x18\x01 \x01(\t\x12\x10\n\x08lastName\x18\x02 \x01(\t\x1aJ\n\x0fLobbyMoneySpent\x12\r\n\x05start\x18\x01 \x01(\x05\x12\n\n\x02to\x18\x02 \x01(\x05\x12\x0c\n\x04year\x18\x03 \x01(\t\x12\x0e\n\x06reason\x18\x04 \x01(\tb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'bakdata.lobby.v1.lobby_pb2', globals())
@@ -21,5 +21,11 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _LOBBY._serialized_start=51
-  _LOBBY._serialized_end=183
+  _LOBBY._serialized_end=560
+  _LOBBY_LOBBYEMPLOYEECOUNT._serialized_start=383
+  _LOBBY_LOBBYEMPLOYEECOUNT._serialized_end=430
+  _LOBBY_EMPLOYEENAMES._serialized_start=432
+  _LOBBY_EMPLOYEENAMES._serialized_end=484
+  _LOBBY_LOBBYMONEYSPENT._serialized_start=486
+  _LOBBY_LOBBYMONEYSPENT._serialized_end=560
 # @@protoc_insertion_point(module_scope)
--- lobby_crawler/lobby_extractor.py
+++ lobby_crawler/lobby_extractor.py
@@ -13,26 +13,65 @@ log = logging.getLogger(__name__)
 class LobbyExtractor:
     def __init__(self):
         self.producer = LobbyProducer()
+        self.url = f"https://www.lobbyregister.bundestag.de/sucheDetailJson?sort=REGISTRATION_DESC"
 
     def extract(self):
-        lobbydatafile = LobbyExtractor.send_request()
+        lobbydatafile = requests.get(url=self.url)
+        results = list(lobbydatafile["results"])
         # nehme Json Datei, in results und dort über Liste iterieren
         # -> in lobby.proto passendes Schema erstellen (überlegen welche Daten wir wollen)
         # aus jedem Objekt in der Liste die entsprechenden Attribute mit dem proto Schema mappen
-        for (lobbyEntry in lobbydatafile["results"]):
+        for lobbyEntry in results:
             try:
-                lobby = Lobby()
-                lobby.id = lobbyEntry.registerNumber
-                # more
+                if lobbyEntry["lobbyistIdentity"]["identity"]  == "ORGANIZATION":
+                    lobby = Lobby()
+                    lobby.id = lobbyEntry["registerNumber"]
+                    
+                    lobbyEntryDetails = lobbyEntry["registerEntryDetail"]
+                    
+                    lobby.companyName = lobbyEntryDetails["lobbyistIdentity"]["name"]
+                    lobby.lobbyEmployeeCount.start = lobbyEntryDetails["employeeCount"]["from"]
+                    lobby.lobbyEmployeeCount.to = lobbyEntryDetails["employeeCount"]["to"]
+                    employees = lobbyEntryDetails["namedEmployees"]
+                    for employee in employees:
+                        lobby.lobbyEmployyeNames.firstName = employee["commenFirstName"]
+                        lobby.lobbyEmployyeNames.lastName = employee["lastName"]
+                    lobby.reasonForLobbying = lobbyEntryDetails["activityDescription"]
+                    #ToDO
+                    lobby.memberships = lobbyEntryDetails["lobbyistIdentity"]["membershipEntries"]
+                    if lobbyEntryDetails["refuseDonationInformation"] or not lobbyEntryDetails["donationInformationRequired"]:
+                        log.debug("no donator informaton", lobbyEntryDetails)
+                        lobby.donators = None
+                    else:
+                        donators = lobbyEntryDetails["donators"]
+                        donatorsName = []
+                        for donator in donators:
+                            donatorsName.append(donator["name"])
+                        if donatorsName != []:
+                            lobby.donators = donatorsName
+                        else: 
+                            lobby.donators = None
+                    lobby.intrests
+                    if lobbyEntryDetails["refuseFinancialExpensesInformation"]:
+                        log.debug("no Finance Info")
+                        lobby.LobbyingMonneySpend.start = None
+                        lobby.LobbyingMonneySpend.to = None
+                        lobby.LobbyingMonneySpend.year = None
+                        lobby.LobbyingMonneySpend.reason = lobbyEntryDetails["refuseFinancialExpensesInformationReason"]
+                    else:
+                        financialExpenses = lobbyEntryDetails["financialExpensesEuro"]
+                        lobby.LobbyingMonneySpend.start = financialExpenses["from"]
+                        lobby.LobbyingMonneySpend.to = financialExpenses["to"]
+                        yearString = financialExpenses["fiscalYearStart"]
+                        lobby.LobbyingMonneySpend.year = "20"+yearString[yearString.find("/")+1:]
+                        lobby.LobbyingMonneySpend.reason = None
 
-                self.producer.produce_to_topic(lobby=lobby)
-                log.debug(lobby)
+                    # more
+
+                    self.producer.produce_to_topic(lobby=lobby)
+                    log.debug(lobby)
+                else:
+                    log.debug("natural person does not get scraped",lobbyEntryDetails)
             except Exception as ex:
                 log.error(f"Error: {ex}")
-                continue
         exit(0)
-        
-    @staticmethod
-    def send_request() -> str:
-        url = f"https://www.lobbyregister.bundestag.de/sucheDetailJson?sort=REGISTRATION_DESC"
-        return requests.get(url=url)
