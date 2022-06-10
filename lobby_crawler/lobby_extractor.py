31e17ab3b440da104a989aec625d05f4618e9178
bjoeh98 <bjoeh98@mi.fu-berlin.de>
2022-06-10 15:38:05 -0500

    update

--- lobby_crawler/lobby_extractor.py
+++ lobby_crawler/lobby_extractor.py
@@ -3,10 +3,9 @@ from time import sleep
 
 import requests
 from parsel import Selector
-
 from build.gen.bakdata.lobby.v1.lobby_pb2 import Lobby
 from lobby_producer import LobbyProducer
-import json 
+import json
 
 log = logging.getLogger(__name__)
 
@@ -17,71 +16,62 @@ class LobbyExtractor:
         self.url = f"https://www.lobbyregister.bundestag.de/sucheDetailJson?sort=REGISTRATION_DESC"
 
     def extract(self):
-        lobbydatafile = requests.get(url=self.url).json()
-        results = lobbydatafile["results"]
+        lobbydatafile = json.load(requests.get(url=self.url))
+        results = list(lobbydatafile["results"])
         # nehme Json Datei, in results und dort über Liste iterieren
         # -> in lobby.proto passendes Schema erstellen (überlegen welche Daten wir wollen)
         # aus jedem Objekt in der Liste die entsprechenden Attribute mit dem proto Schema mappen
         for lobbyEntry in results:
-            # try:
-            lobbyEntryDetails = lobbyEntry["registerEntryDetail"] 
-            print(lobbyEntry)
-            if lobbyEntryDetails["lobbyistIdentity"]["identity"]  == "ORGANIZATION":
-                lobby = Lobby()
-                lobby.id = lobbyEntry["registerNumber"]
-                
-               
-                lobby.companyName = lobbyEntryDetails["lobbyistIdentity"]["name"]
-                lobby.lobbyEmployeeCount.start = lobbyEntryDetails["employeeCount"]["from"]
-                lobby.lobbyEmployeeCount.to = lobbyEntryDetails["employeeCount"]["to"]
-                employees = lobbyEntryDetails["lobbyistIdentity"]["namedEmployees"]
-                employeeNames = []
-                for employee in employees:
-                    employeeNames.append(employee["lastName"] + ", " + employee["commonFirstName"])
-                lobby.lobbyEmployyeNames.extend(employeeNames)
-                lobby.reasonForLobbying = lobbyEntryDetails["activityDescription"]
-                #ToDO
-                lobby.memberships.extend(lobbyEntryDetails["lobbyistIdentity"]["membershipEntries"])
-                try:
+            try:
+                if lobbyEntry["lobbyistIdentity"]["identity"]  == "ORGANIZATION":
+                    lobby = Lobby()
+                    lobby.id = lobbyEntry["registerNumber"]
+                    
+                    lobbyEntryDetails = lobbyEntry["registerEntryDetail"]
+                    
+                    lobby.companyName = lobbyEntryDetails["lobbyistIdentity"]["name"]
+                    lobby.lobbyEmployeeCount.start = lobbyEntryDetails["employeeCount"]["from"]
+                    lobby.lobbyEmployeeCount.to = lobbyEntryDetails["employeeCount"]["to"]
+                    employees = lobbyEntryDetails["namedEmployees"]
+                    for employee in employees:
+                        lobby.lobbyEmployyeNames.firstName = employee["commenFirstName"]
+                        lobby.lobbyEmployyeNames.lastName = employee["lastName"]
+                    lobby.reasonForLobbying = lobbyEntryDetails["activityDescription"]
+                    #ToDO
+                    lobby.memberships = lobbyEntryDetails["lobbyistIdentity"]["membershipEntries"]
                     if lobbyEntryDetails["refuseDonationInformation"] or not lobbyEntryDetails["donationInformationRequired"]:
                         log.debug("no donator informaton", lobbyEntryDetails)
-                        lobby.donators.extend([])
+                        lobby.donators = None
                     else:
                         donators = lobbyEntryDetails["donators"]
                         donatorsName = []
                         for donator in donators:
                             donatorsName.append(donator["name"])
-                        lobby.donators.extend(donatorsName)
-                except:
-                    log.debug("no donator informaton", lobbyEntryDetails)
-                    lobby.donators.extend([])
-                try:
+                        if donatorsName != []:
+                            lobby.donators = donatorsName
+                        else: 
+                            lobby.donators = None
+                    lobby.intrests
                     if lobbyEntryDetails["refuseFinancialExpensesInformation"]:
                         log.debug("no Finance Info")
-                        lobby.lobbyMoneySpent.start = 0
-                        lobby.lobbyMoneySpent.to = 0
-                        lobby.lobbyMoneySpent.year = ""
-                        lobby.lobbyMoneySpent.reason = lobbyEntryDetails["refuseFinancialExpensesInformationReason"]
+                        lobby.LobbyingMonneySpend.start = None
+                        lobby.LobbyingMonneySpend.to = None
+                        lobby.LobbyingMonneySpend.year = None
+                        lobby.LobbyingMonneySpend.reason = lobbyEntryDetails["refuseFinancialExpensesInformationReason"]
                     else:
                         financialExpenses = lobbyEntryDetails["financialExpensesEuro"]
-                        lobby.lobbyMoneySpent.start = financialExpenses["from"]
-                        lobby.lobbyMoneySpent.to = financialExpenses["to"]
+                        lobby.LobbyingMonneySpend.start = financialExpenses["from"]
+                        lobby.LobbyingMonneySpend.to = financialExpenses["to"]
                         yearString = financialExpenses["fiscalYearStart"]
-                        lobby.lobbyMoneySpent.year = "20"+yearString[yearString.find("/")+1:]
-                        lobby.lobbyMoneySpent.reason = ""
-                except:
-                    log.debug("no Finance Info")
-                    lobby.lobbyMoneySpent.start = 0
-                    lobby.lobbyMoneySpent.to = 0
-                    lobby.lobbyMoneySpent.year = ""
-                    lobby.lobbyMoneySpent.reason = ""
+                        lobby.LobbyingMonneySpend.year = "20"+yearString[yearString.find("/")+1:]
+                        lobby.LobbyingMonneySpend.reason = None
 
-                # more
+                    # more
 
-                self.producer.produce_to_topic(lobby=lobby)
-                log.debug(lobby)
-            else:
-                log.debug("natural person does not get scraped",lobbyEntryDetails)
-            # except Exception as ex:
-            #     log.error(f"Error: {ex}")
+                    self.producer.produce_to_topic(lobby=lobby)
+                    log.debug(lobby)
+                else:
+                    log.debug("natural person does not get scraped",lobbyEntryDetails)
+            except Exception as ex:
+                log.error(f"Error: {ex}")
         exit(0)
