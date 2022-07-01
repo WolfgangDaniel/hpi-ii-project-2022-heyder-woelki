f33334ed7e0cec941709e5493a0dc0ccdf3269d1
WolfgangDaniel <42138601+WolfgangDaniel@users.noreply.github.com>
2022-07-01 16:20:30 +0200

    produce elastic data to kafka

--- rb_crawler/main.py
+++ rb_crawler/main.py
@@ -12,16 +12,8 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option("-i", "--id", "rb_id", type=int, help="The rb_id to initialize the crawl from")
-@click.option("-s", "--state", type=click.Choice(State), help="The state ISO code")
-def run(rb_id: int, state: State):
-    if state == State.SCHLESWIG_HOLSTEIN:
-        if rb_id < 7830:
-            error = ValueError("The start rb_id for the state SCHLESWIG_HOLSTEIN (sh) is 7831")
-            log.error(error)
-            exit(1)
-    RbExtractor(rb_id, state.value).extract()
+def run():
+    RbExtractor().extract()
 
 
 if __name__ == "__main__":
--- rb_crawler/rb_extractor.py
+++ rb_crawler/rb_extractor.py
@@ -1,3 +1,5 @@
+import json
+import ijson
 import logging
 from time import sleep
 
@@ -13,37 +15,64 @@ log = logging.getLogger(__name__)
 
 
 class RbExtractor:
-    def __init__(self, start_rb_id: int, state: str):
-        self.rb_id = start_rb_id
-        self.state = state
+    def __init__(self):
         self.producer = RbProducer()
 
     def extract(self):
-        while True:
-            try:
-                log.info(f"Sending Request for: {self.rb_id} and state: {self.state}")
-                text = self.send_request()
-                if "Falsche Parameter" in text:
-                    log.info("The end has reached")
-                    break
-                selector = Selector(text=text)
-                corporate = Corporate()
-                corporate.rb_id = self.rb_id
-                corporate.state = self.state
-                corporate.reference_id = self.extract_company_reference_number(selector)
-                event_type = selector.xpath("/html/body/font/table/tr[3]/td/text()").get()
-                corporate.event_date = selector.xpath("/html/body/font/table/tr[4]/td/text()").get()
-                corporate.id = f"{self.state}_{self.rb_id}"
-                raw_text: str = selector.xpath("/html/body/font/table/tr[6]/td/text()").get()
-                self.handle_events(corporate, event_type, raw_text)
-                self.rb_id = self.rb_id + 1
-                log.debug(corporate)
-            except Exception as ex:
-                log.error(f"Skipping {self.rb_id} in state {self.state}")
-                log.error(f"Cause: {ex}")
-                self.rb_id = self.rb_id + 1
-                continue
-        exit(0)
+        with open('exports\corporate-events-dump.json', 'r') as rb_data:
+                line = rb_data.readline()
+                while line:
+                    try:
+                        line_obj = json.loads(line)
+                        corporate_event = line_obj['_source']
+                        # print(line_obj['_source'])
+                        corporate = Corporate()
+                        corporate.rb_id = corporate_event['rb_id']
+                        corporate.state = corporate_event['state']
+                        corporate.reference_id = corporate_event['reference_id']
+                        event_type = corporate_event['event_type']
+                        corporate.event_date = corporate_event['event_date']
+                        corporate.id = corporate_event['id']
+                        raw_text: str = corporate_event['information']
+                        self.handle_elastic_events(corporate, event_type, raw_text, corporate_event['state'])
+                        line = rb_data.readline()
+                    except Exception as ex:
+                        log.error(f"Cause: {ex}")
+                        line = rb_data.readline()
+                        continue
+        
+        # counter = 0
+        # with open('exports\corporate-events-dump.json', 'rb') as data:
+        #     for corporate_event in ijson.items(data, 'item'):
+        #         counter = counter + 1
+        #         print(corporate_event)
+        #         print("Finished object " + str(counter))
+
+        # while True:
+        #     try:
+        #         log.info(f"Sending Request for: {self.rb_id} and state: {self.state}")
+        #         text = self.send_request()
+        #         if "Falsche Parameter" in text:
+        #             log.info("The end has reached")
+        #             break
+        #         selector = Selector(text=text)
+        #         corporate = Corporate()
+        #         corporate.rb_id = self.rb_id
+        #         corporate.state = self.state
+        #         corporate.reference_id = self.extract_company_reference_number(selector)
+        #         event_type = selector.xpath("/html/body/font/table/tr[3]/td/text()").get()
+        #         corporate.event_date = selector.xpath("/html/body/font/table/tr[4]/td/text()").get()
+        #         corporate.id = f"{self.state}_{self.rb_id}"
+        #         raw_text: str = selector.xpath("/html/body/font/table/tr[6]/td/text()").get()
+        #         self.handle_events(corporate, event_type, raw_text)
+        #         self.rb_id = self.rb_id + 1
+        #         log.debug(corporate)
+        #     except Exception as ex:
+        #         log.error(f"Skipping {self.rb_id} in state {self.state}")
+        #         log.error(f"Cause: {ex}")
+        #         self.rb_id = self.rb_id + 1
+        #         continue
+        # exit(0)
 
     def send_request(self) -> str:
         url = f"https://www.handelsregisterbekanntmachungen.de/skripte/hrb.php?rb_id={self.rb_id}&land_abk={self.state}"
@@ -57,18 +86,24 @@ class RbExtractor:
 
     def handle_events(self, corporate, event_type, raw_text):
         if event_type == "Neueintragungen":
-            self.handle_new_entries(corporate, raw_text)
+            self.handle_new_entries(corporate, raw_text, )
         elif event_type == "Veränderungen":
             self.handle_changes(corporate, raw_text)
         elif event_type == "Löschungen":
             self.handle_deletes(corporate)
 
-    def handle_new_entries(self, corporate: Corporate, raw_text: str) -> Corporate:
+    def handle_elastic_events(self, corporate, event_type, raw_text, state):
+        if event_type == "create":
+            self.handle_new_entries(corporate, raw_text, state)
+        elif event_type == "delete":
+            self.handle_deletes(corporate)
+
+    def handle_new_entries(self, corporate: Corporate, raw_text: str, state: str) -> Corporate:
         log.debug(f"New company found: {corporate.id}")
         corporate.event_type = "create"
         corporate.information = raw_text
         corporate.status = Status.STATUS_ACTIVE
-        info = parse(raw_text,self.state)
+        info = parse(raw_text, state)
         corporate.company_name = info['company_name']
         corporate.address_street = info['street']
         corporate.address_plz = info['plz']
